135dbde0851fd3201fba08964c90b88f5b81d17f
Radek Vala <radek.www@gmail.com>
2024-04-19 18:16:36 +0200

    Initiate the pinia state management and persistence plugin

--- src/main.js
+++ src/main.js
@@ -1,14 +1,20 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
+// persistant state plugin for pinia (instalation: npm i pinia-plugin-persistedstate)
+import piniaPluginPersistedstate from "pinia-plugin-persistedstate";
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
+// initialize Pinia
+const pinia = createPinia();
+// use persistance pinina plugin
+pinia.use(piniaPluginPersistedstate);
 
-app.use(createPinia())
+app.use(pinia);
 app.use(router)
 
 app.mount('#app')
